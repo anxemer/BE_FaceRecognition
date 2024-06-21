54ca9591d254db59c5cbf186f5cb1cbf7d13bbc7
Lê Hoàng Phúc <147243750+PhucLHSE@users.noreply.github.com>
2024-06-21 23:35:30 +0700

    Add StudentInCourseDTO

---
+++ CheckStudent.Repository/DTO/StudentInCourse/StudentInCourseDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckStudent.Repository.DTO.StudentInCourse
+{
+    public class StudentInCourseDTO
+    {
+        public int Id { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public int? Grade { get; set; }
+        public string Note { get; set; }
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+    }
+}
--- CheckStudent.Repository/obj/Debug/net8.0/CheckStudent.Repository.AssemblyInfo.cs
+++ CheckStudent.Repository/obj/Debug/net8.0/CheckStudent.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CheckStudent.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13a44a0bd74a174720d7155fb14b71e6f94ef99d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be41f0cc52bb44b79847c38c4a5dd2deffdae71d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CheckStudent.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CheckStudent.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
