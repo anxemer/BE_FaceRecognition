c972f659662cbe0f40b3dcd3b48ce5a39d91a7b5
Lê Hoàng Phúc <147243750+PhucLHSE@users.noreply.github.com>
2024-07-02 09:16:02 +0700

    Fix Put method error

--- CheckStudent.API/obj/Debug/net8.0/CheckStudent.API.AssemblyInfo.cs
+++ CheckStudent.API/obj/Debug/net8.0/CheckStudent.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CheckStudent.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76be21948466ea4b705fdb40381c3e66468f9f93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ad50a00c054292dda83172bfc40c9eee3412889")]
 [assembly: System.Reflection.AssemblyProductAttribute("CheckStudent.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CheckStudent.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
